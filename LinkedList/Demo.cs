666e47159ed1d20dbe6b3519909bdb35277d796f
Suresh-Thota <Suresh.Thota@innroad.com>
2020-07-29 18:48:17 +0530

    commit changes

--- LinkedList/Demo.cs
+++ LinkedList/Demo.cs
@@ -6,67 +6,147 @@ namespace LinkedList
 {
     class Demo
     {
-        static void Main(String[] arg)
+        static void Main(string[] args)
         {
+            int choice, data, k, x;
             SingleLinkedList list = new SingleLinkedList();
-
             list.CreateList();
-            list.Display();
-            list.CountNode();
-            int x = 0;
-            Console.WriteLine("Enter an element to search");
-            x = Convert.ToInt32(Console.ReadLine());
-            list.Search(x);
-            list.ReferenceAtLastNode();
-            list.ReferenceAtSecondLastNode();
-            int y = 0;
-            Console.WriteLine("Enter an element to get predecessor of that node");
-            y = Convert.ToInt32(Console.ReadLine());
-            list.predecessorOfGivenNode(y);
-            int z = 0;
-            Console.WriteLine("Enter an element to get node");
-            z = Convert.ToInt32(Console.ReadLine());
-            list.NodeAtParticularPosition(z);
-            int a = 0;
-            Console.WriteLine("enter an element ");
-            a = Convert.ToInt32(Console.ReadLine());
-            list.InsertAtBeginningToList(a);
-            //------------------------------------------------------------------
-            //------------------------------
-            int data, b;
-            Console.WriteLine("select a node");
-            b = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("enter a new node to insert after " + b + " ");
-            data = Convert.ToInt32(Console.ReadLine());
-            list.InsertAfter(data, b);
-            //-------------------------------------------------------------------
-            int data1, c;
-            Console.WriteLine("select a node");
-            c = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("enter a new node to insert before " + c + " ");
-            data1 = Convert.ToInt32(Console.ReadLine());
-            list.InsertBefore(data1, c);
 
-            //----------------------------------------------------------------------
-            int data2, d;
-            Console.WriteLine("enter the location");
-            d = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(" enter a new node to insert at " + d);
-            data2 = Convert.ToInt32(Console.ReadLine());
-            list.InsertAtPosition(data2, d);
-            //---------------------------------------------------------------------
-            list.DeleteFirstNode();
-            //----------------------------------------------------------------
-            list.DeleteLastode();
-            //---------------------------------------------------------------------
-            int e;
-            Console.WriteLine("enter a node that you want to delete");
-            e = Convert.ToInt32(Console.ReadLine());
-            list.deleteNode(e);
-            //--------------------------------------------------------------------
-            list.ReverseList();
-            //-------------------------------------------------------------------
-            list.BubbleSort();
+            while (true)
+            {
+                Console.WriteLine("1.Display List");
+                Console.WriteLine("2.Count the number of nodes");
+                Console.WriteLine("3.search for an element");
+                Console.WriteLine("4.Insert in empty list");
+                Console.WriteLine("5.Insert a node at the end of list ");
+                Console.WriteLine("6.Insert a node after a specified node");
+                Console.WriteLine("7.Insert a node before a specified node");
+                Console.WriteLine("8.Insert a node at a given position");
+                Console.WriteLine("9.Delete first node");
+                Console.WriteLine("10.Delete last node");
+                Console.WriteLine("11.Delete any node");
+                Console.WriteLine("12.Reverse the last");
+                Console.WriteLine("13.Bubble sort by exchanging data");
+                Console.WriteLine("14.Merging with two lists");
+                Console.WriteLine("15.Merge sort");
+                Console.WriteLine("16.Insert cycle");
+                Console.WriteLine("17.delete cycle");
+                Console.WriteLine("18.Remove cycle");
+                Console.WriteLine("19.Quit");
+                Console.Write("Enter your choice:");
+                choice = Convert.ToInt32(Console.ReadLine());
+                if (choice == 19)
+                    break;
+                switch (choice)
+                {
+                    case 1:
+                        list.Display();
+                        break;
+                    case 2:
+                        list.CountNode();
+                        break;
+                    case 3:
+                        Console.Write("Enter element to be searched");
+                        data = Convert.ToInt32(Console.ReadLine());
+                        list.Search(data);
+                        break;
+                    case 4:
+                        Console.WriteLine("Enter Element to be inserted");
+                        data = Convert.ToInt32(Console.ReadLine());
+                        list.InsertAtBeginningToList(data);
+                        break;
+                    case 5:
+                        Console.WriteLine("Enter Element to be inserted");
+                        data = Convert.ToInt32(Console.ReadLine());
+                        list.InsertAtEnd(data); ;
+                        break;
+                    case 6:
+                        Console.WriteLine("Enter Element to be inserted");
+                        data = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter Element after which to be inserted:");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        list.InsertAfter(data, x);
+                        break;
+                    case 7:
+                        Console.WriteLine("Enter Element to be inserted");
+                        data = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter Element before which to be inserted:");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        list.InsertBefore(data, x);
+                        break;
+                    case 8:
+                        Console.WriteLine("Enter Element to be inserted");
+                        data = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter position which to be inserted:");
+                        k = Convert.ToInt32(Console.ReadLine());
+                        list.InsertAtPosition(data, k);
+                        break;
+                    case 9:
+                        list.DeleteFirstNode();
+                        break;
+                    case 10:
+                        list.DeleteLastode();
+                        break;
+                    case 11:
+                        Console.WriteLine("Enter Element to be deleted:");
+                        data = Convert.ToInt32(Console.ReadLine());
+                        list.deleteNode(data);
+                        break;
+                    case 12:
+                        list.ReverseList();
+                        break;
+                    case 13:
+                        list.BubbleSort();
+                        break;
+                    case 14:
+                        // list.BubbleSortExLinks();
+                        SingleLinkedList list1 = new SingleLinkedList();
+                        SingleLinkedList list2 = new SingleLinkedList();
+
+                        list1.CreateList();
+                        list2.CreateList();
+
+                        list1.BubbleSort();
+                        list2.BubbleSort();
+
+                        Console.WriteLine(" First List:");
+                        list1.Display();
+                        Console.WriteLine(" Second List");
+                        list2.Display();
+
+                        SingleLinkedList list3;
+                        list3 = list1.Merge1(list2);
+                        break;
+                    case 15:
+                        list.MergeSort();
+                        break;
+                    case 16:
+                        Console.WriteLine("Enter element at which cycle has to be inserted  ");
+                        data = Convert.ToInt32(Console.ReadLine());
+                        list.InsertCycle(data);
+                        break;
+                    case 17:
+                        if (list.HasCycle())
+                            Console.WriteLine("List has a cycle");
+                        else
+                            Console.WriteLine("List does not  have a cycle");
+                        break;
+                    case 18:
+                        list.RemoveCycle();
+                        break;
+                    default:
+                        Console.WriteLine("Wrong choice:");
+                        break;
+
+
+
+                }
+                Console.WriteLine();
+
+
+            }
+            Console.WriteLine("Existing");
         }
     }
+
 }
--- LinkedList/SingleLinkedList.cs
+++ LinkedList/SingleLinkedList.cs
@@ -309,6 +309,210 @@ namespace LinkedList
             }
              
         }
-        
+        public SingleLinkedList Merge1(SingleLinkedList list)
+        {
+            SingleLinkedList mergeList = new SingleLinkedList();
+            mergeList.start = Merge1(start, list.start);
+            return mergeList;
+        }
+        private Node Merge1(Node p,Node q)
+        {
+            Node startM;
+            if(p.info <= p.info)
+            {
+                startM = new Node(p.info);
+                p = p.link;
+            }
+            else
+            {
+                startM = new Node(q.info);
+                q = q.link;
+            }
+            Node pm = startM;
+
+            while(p != null && q != null)
+            {
+                if (p.info < q.info)
+                {
+                    pm.link = new Node(p.info);
+                    p = p.link;
+                }
+                else
+                {
+                    pm.link = new Node(q.info);
+                    q = q.link;
+                }
+
+            }
+
+            while (p != null)
+            {
+                pm.link = new Node(p.info);
+                p = p.link;
+                pm = pm.link;
+            }
+            while (q != null)
+            {
+                pm.link = new Node(q.info);
+                q = q.link;
+                pm = pm.link;
+            }
+            return startM;
+        }
+
+        public SingleLinkedList Merge2(SingleLinkedList list)
+        {
+            SingleLinkedList mergelist = new SingleLinkedList();
+            mergelist.start = Merge2(start, list.start);
+            return mergelist;
+        }
+        private Node Merge2(Node p1,Node p2)
+        {
+            Node startM;
+            if(p1.info < p2.info)
+            {
+                startM = p1;
+                p1 = p1.link;
+            }
+            else
+            {
+                startM = p2;
+                p2 = p2.link;
+            }
+            Node pm = startM;
+            while (p1 != null && p2 != null)
+            {
+                if (p1.info <= p2.info)
+                {
+                    pm.link = p1;
+                    pm = pm.link;
+                    p1 = p1.link;
+                }
+                else
+                {
+                    pm.link = p2;
+                    pm = pm.link;
+                    p2 = p2.link;
+                }
+            }
+            if(p1 == null)
+            {
+                pm.link = p2;
+            }
+            else
+            {
+                pm.link = p1;
+            }
+            return startM;
+        }
+        public void MergeSort()
+        {
+            start = MergeSortRec(start);
+        }
+        private Node MergeSortRec(Node listStart)
+        {
+            if (listStart == null || listStart.link == null)
+            {
+                return listStart;
+            }
+            Node start1 = listStart;
+            Node start2 = DivideList(listStart);
+            start1 = MergeSortRec(start1);
+            start2 = MergeSortRec(start2);
+            Node startM = Merge2(start1, start2);
+            return startM;
+        }
+        private Node DivideList(Node p)
+        {
+            Node q = p.link.link;
+            while (q != null && q.link != null)
+            {
+                p = p.link;
+                q = p.link.link;
+            }
+            Node start2 = p.link;
+            p.link = null;
+            return start2;
+        }
+        public bool HasCycle()
+        {
+            if (FindCycle() == null)
+            {
+                return false;
+            }
+            else
+                return true;
+        }
+        private Node FindCycle()
+        {
+            if (start == null || start.link == null)
+                return null;
+            Node slowR = start, fastR = start;
+            while(fastR != null && fastR.link != null)
+            {
+                slowR = slowR.link;
+                fastR = fastR.link.link;
+                if (slowR == fastR)
+                {
+                    return slowR;
+                }
+            }
+            return null;
+        }
+        public void RemoveCycle()
+        {
+            Node c = FindCycle();
+            if (c == null)
+                return;
+            Console.WriteLine("Node at which the cycle was detected is :" + c.info);
+            Node p = c, q = c;
+            int lenCycle = 0;
+            do
+            {
+                lenCycle++;
+                q = q.link;
+            } while (q != p);
+            Console.WriteLine("length of Cycle is :" + lenCycle);
+            int lenRemList = 0;
+            p = start;
+            while (p != q)
+            {
+                lenRemList++;
+                p = p.link;
+                q = q.link;
+            }
+            Console.WriteLine("number of Nodes not included in the Cycle are :" + lenRemList);
+
+            int lengthOfList = lenCycle + lenRemList;
+            Console.WriteLine("Length of the List :" + lengthOfList);
+            p = start;
+            for(int i = 1; i <= lengthOfList - 1; i++)
+            {
+                p = p.link;
+            }
+            p.link = null;      
+        }
+        public void InsertCycle(int x)
+        {
+            if (start == null)
+                return;
+            Node p = start, px = null, prev = null;
+            while (p != null)
+            {
+                if (p.info == x)
+                    px = p;
+                prev = p;
+                p = p.link;
+            }
+            if (px != null)
+            {
+                prev.link = px;
+            }
+            else
+            {
+                Console.WriteLine(x + " not present in the list");
+            }
+        }
+
     }
 }
